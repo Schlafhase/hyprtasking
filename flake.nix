9459a13813ddb435be1f88a50a5ee4140921850c
Fazzi <faaris.ansari@proton.me>
2026-01-15 20:56:44 +0000

    nix: expose two seperate packages

--- flake.nix
+++ flake.nix
@@ -23,26 +23,8 @@
       );
   in {
     packages = forSystems (system: pkgs: {
-      hyprtasking = let
-        hyprlandPkg = hyprland.packages.${system}.hyprland;
-      in
-        pkgs.gcc14Stdenv.mkDerivation {
-          pname = "hyprtasking";
-          version = "0.1";
-
-          src = ./.;
-
-          nativeBuildInputs = [pkgs.meson pkgs.ninja] ++ hyprlandPkg.nativeBuildInputs;
-          buildInputs = [hyprlandPkg] ++ hyprlandPkg.buildInputs;
-
-          meta = with lib; {
-            homepage = "https://github.com/raybbian/hyprtasking";
-            description = "Powerful workspace management plugin, packed with features ";
-            license = licenses.bsd3;
-            platforms = platforms.linux;
-          };
-        };
-
+      hyprtaskingStable = pkgs.callPackage ./nix/package.nix {};
+      hyprtasking = pkgs.callPackage ./nix/package.nix {hyprland = hyprland.packages.${system}.default;};
       default = self.packages.${system}.hyprtasking;
     });
   };
